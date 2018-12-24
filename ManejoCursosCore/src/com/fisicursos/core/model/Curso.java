6bb4df5cafb53326d2f751c1419dc706547c8fd3
alejo <alejonc15@gmail.com>
2018-12-24 12:54:57 -0500

    agregando las clases al model

---
+++ ManejoCursosCore/src/com/fisicursos/core/model/Curso.java
@@ -0,0 +1,76 @@
+package com.fisicursos.core.model;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Curso {
+    
+    private int codigo;
+    private String codigoBase;
+    private String nombre;
+    private Plan plan;
+    private int ciclo;
+    private int creditos;
+
+    public Curso(int codigo, String codigoBase, String nombre, Plan plan, int ciclo, int creditos) {
+        this.codigo = codigo;
+        this.codigoBase = codigoBase;
+        this.nombre = nombre;
+        this.plan = plan;
+        this.ciclo = ciclo;
+        this.creditos = creditos;
+    }
+
+    public Curso() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCodigoBase() {
+        return codigoBase;
+    }
+
+    public void setCodigoBase(String codigoBase) {
+        this.codigoBase = codigoBase;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Plan getPlan() {
+        return plan;
+    }
+
+    public void setPlan(Plan plan) {
+        this.plan = plan;
+    }
+
+    public int getCiclo() {
+        return ciclo;
+    }
+
+    public void setCiclo(int ciclo) {
+        this.ciclo = ciclo;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+    
+}
---
+++ ManejoCursosCore/src/com/fisicursos/core/model/Periodo.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fisicursos.core.model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Periodo {
+    
+    private int idPeriodo;
+    private String nombre;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+
+    public Periodo(int idPeriodo, String nombre, LocalDate fechaInicio, LocalDate fechaFin) {
+        this.idPeriodo = idPeriodo;
+        this.nombre = nombre;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+
+    public Periodo() {
+    }
+
+    public int getIdPeriodo() {
+        return idPeriodo;
+    }
+
+    public void setIdPeriodo(int idPeriodo) {
+        this.idPeriodo = idPeriodo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    
+    
+}
